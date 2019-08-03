e982beb7604ad8ae9d6c78c083cc67271994d13c
Matheus Boni Vicari <matheus.boni.vicari@gmail.com>
2019-08-02 21:38:43 -0300

    Added multiprocessing to iterative ICP
    * Using Python multiprocessing module to detect number of CPUs and use parallelism to process combinations of key points/base points.

--- src/coarse_registration.py
+++ src/coarse_registration.py
@@ -10,6 +10,8 @@ import numpy as np
 from icp import icp
 from ransac_circle import RANSAC
 import transformations as tt
+from multiprocessing import Pool, cpu_count
+
 
 def iterative_icp(key_points, base_points, max_matches=7, min_matches=3,
                   max_plane_rot=0.2, max_transl=1):
@@ -21,32 +23,44 @@ def iterative_icp(key_points, base_points, max_matches=7, min_matches=3,
         
     key_ids = np.arange(key_points.shape[0], dtype=int)
     base_ids = np.arange(base_points.shape[0], dtype=int)
-    
-    min_error = np.inf
-    optimum_T = np.eye(4)
-    total_matches = np.inf
+      
+    point_combinations = []
     for sn in sample_numbers[::-1]:
         for key_sids in it.combinations(key_ids, sn):
             key_sids = np.array(key_sids)
             for base_sids in it.combinations(base_ids, sn):
                 base_sids = np.array(base_sids)
-                T, error, i = icp(key_points[key_sids], base_points[base_sids])
-                mean_error = np.mean(error)
+                point_combinations.append([key_points[key_sids],
+                                           base_points[base_sids]])
                 
-                rotation_angles = tt.euler_from_matrix(T)
-                max_plane_angle =  np.max(np.abs(rotation_angles[:2]))
-                
-                max_T_transl = np.max(T[:3, 3])
-                
-                if (max_plane_angle <= max_plane_rot) & (max_T_transl <= max_transl):
-                    if mean_error < min_error:
-                        min_error = mean_error
-                        optimum_T = T
-                        total_matches = sn
+    n_cpus = cpu_count()
+    p = Pool(processes=n_cpus)
+    result = p.map(run_icp_process, point_combinations)
+
+    min_error = np.inf
+    optimum_T = np.eye(4)
+    total_matches = np.inf 
+    for T, mean_error, sn in result:   
+        rotation_angles = tt.euler_from_matrix(T)
+        max_plane_angle =  np.max(np.abs(rotation_angles[:2]))
+        
+        max_T_transl = np.max(T[:3, 3])
+        
+        if (max_plane_angle <= max_plane_rot) & (max_T_transl <= max_transl):
+            if mean_error < min_error:
+                min_error = mean_error
+                optimum_T = T
+                total_matches = sn
                     
     return optimum_T, min_error, total_matches
 
+
+def run_icp_process(point_combinations):
+    T, error, i = icp(point_combinations[0], point_combinations[1])
+    mean_error = np.mean(error)
+    return T, mean_error, point_combinations[0].shape[0]
     
+
 def fit_circle(x, y, n_iter=100):
     
     """
